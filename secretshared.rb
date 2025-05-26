a55050e392d93f470f4c84c535ebd0a3ffa10cfc
evaldo.vega <evaldo.vega@egmail.com>
2025-05-26 16:57:05 -0500

    beta

--- secretshared.rb
+++ secretshared.rb
@@ -3,6 +3,7 @@ class Secretshared < Formula
   homepage "https://github.com/evaldovega/secretshared"
   url "https://github.com/evaldovega/secretshared/releases/download/beta/secretshared"
   sha256 "528859dd974e6ff8b0b38a3d04bcc5c76d50dbdb831cc7ec28f580d5b952a3f6"
+  version "0.0.0"
   license "MIT"
 
   def install
