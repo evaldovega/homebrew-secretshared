c8868ad08a8cc868fe010b43b73220df014d4c53
evaldo.vega <evaldo.vega@egmail.com>
2025-05-26 16:54:25 -0500

    beta

--- secretshared.rb
+++ secretshared.rb
@@ -1,11 +1,12 @@
 class Secretshared < Formula
-    desc "Cli para compartir secretos con todo el equipo devs"
-    homepage "https://github.com/evaldovega/secretshared"
-    url "https://github.com/evaldovega/secretshared/releases/download/productive/secretshared"
-    sha256 "528859dd974e6ff8b0b38a3d04bcc5c76d50dbdb831cc7ec28f580d5b952a3f6"  # Usa `shasum -a 256 archivo.tar.gz` para obtenerlo
-    version "0.1.0"
+  desc "Herramienta para compartir secretos"
+  homepage "https://github.com/evaldovega/secretshared"
+  url "https://github.com/evaldovega/secretshared/releases/download/beta/secretshared"
+  sha256 "528859dd974e6ff8b0b38a3d04bcc5c76d50dbdb831cc7ec28f580d5b952a3f6"
+  license "MIT"
+
+  def install
+    bin.install "secretshared"
+  end
+end
   
-    def install
-      bin.install "secretshared"
-    end
-  end 
